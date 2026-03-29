06ec5f70ddf17e7c1d5287b608ea0ccdfe37b531
Tharindu Munasinghe <tharindu.munasinghe@trycake.com>
2026-03-29 07:27:47 -0500

    Initial Supply chain Provence contract is outlined with following: * User management flow and role based access constructs/modifier * Updated product registration and update functions to handle quantity * Define the product structure with in-chain attributes and hash attribute to link off-chain data * Enables tracking when items gets splitted

---
+++ blockchain/contracts/SupplyChainProvenance.sol
@@ -0,0 +1,164 @@
+
+    
+
+pragma solidity ^0.8.28;
+
+// import "hardhat/console.sol";
+
+contract SupplyChainProvenance {
+
+    // Developer is considerd as the admin,
+    // when the contract is deployed, the deployer address will be assigned with the admin role.
+    // The admin has the authority to assign roles to other users (e.g., producer, distributor, retailer, consumer) 
+    address public admin;
+
+    // New Status of the user before registration, 
+    enum Role { UnRegistered, Producer, Consumer, Distributor, Retailer, Admin, Auditor }
+    
+    // userRoleMapping for authorization and access control 
+    mapping(address => Role) public rolesMapping;
+
+    enum ProductStatus {
+        InProduction, // Product is being manufactured by the producer.
+        ReadyToShip, // Product is ready to be shipped to the warehouse.
+        InTransitToWarehouse, 
+        ShippedToWarehouse,
+        WHQualityCheckPassed, // Product has passed quality check at the warehouse putaway to inventory.
+        ReturnedToProducer, // Product is returned to the producer due to quality check failure at the warehouse.
+        InWarehouse, // Product is stored in the warehouse.
+        InTransitToRetailer,
+        ShippedToRetailer,
+        RetailerQualityCheckPassed,
+        RetailerReturnedToWarehouse,
+        InStore, // Product is on the shelf at the retailer's store.
+        Sold    // product is sold to the end consumer.
+    }
+
+    struct Product {
+        // Unique identifier for the product batch
+        uint256 prodId; 
+        // Address of the producer Static value assigned at creation
+        address producer; 
+        // Address of the producer Static value assigned at creation
+        uint256 producerBatchId;
+        // off-chain data pointer for other information about the product (e.g., manufacturing details, images )
+        string ipfsHash; 
+        uint256 expirationDate;
+
+        // following fields are for tracking the product lifecycle and ownership
+        uint256 currentBatchId; 
+        ProductStatus currentStatus;
+        // Identifier for the current owner (could be producer, distributor, retailer, or consumer)
+        address currentOwner;
+        // Identifier for the parent batch, if batch get splitted into smaller batches at the warehouses  
+        uint256 parentBatchId; 
+    }
+
+
+    // master ledger to store all products 
+    mapping(uint256 => Product) public productLedger;
+
+
+    constructor() {
+        admin = msg.sender;
+        rolesMapping[admin] = Role.Admin;
+    }
+    
+    /************************************************
+    * User Management and Role-Based Access Control
+    *************************************************/
+    
+    /**
+     * @notice Assigns a role to a user address.
+     * @dev Only callable by an account with the Admin role.
+     * @param user The address of the user to assign a role to.
+     * @param role The role to assign to the user.
+     */
+    function assignRole(address user, Role role) public adminOnly {
+        rolesMapping[user] = role;
+    }
+
+
+    /**
+     * @notice Restricts function access to only Admin role.
+     * @dev Reverts if the caller is not an Admin.
+     */
+    modifier adminOnly() {
+        require(rolesMapping[msg.sender] == Role.Admin, "Unauthorized: Only Admin can assign roles");
+        _;
+    }
+
+    /**
+     * @notice Restricts function access to a specific role.
+     * @dev Reverts if the caller does not have the required role.
+     * @param _role The required role for access.
+     */
+    modifier roleCheker(Role _role) {
+        require(rolesMapping[msg.sender] != Role.UnRegistered, "User does not exist.");
+        require(rolesMapping[msg.sender] == _role, "Unauthorized: You do not have the required role. Contact Admin for access.");
+        _;
+    }
+
+
+    /**
+     * @notice Registers the sender as a new user with the default role (UnRegistered).
+     * @dev Reverts if the user is already registered (role is not UnRegistered).
+     */
+    function registerMe() public {
+        require(rolesMapping[msg.sender] == Role.UnRegistered, "You are already registered");
+        rolesMapping[msg.sender] = Role.UnRegistered;
+    }
+    
+
+    /************************************************
+    * Producer Capabilities
+    * (creating product batch, making the product ready for shipping to the warehouse)
+    *************************************************/
+    
+    /**
+     * @notice Registers a new product blockchain.
+     * @dev Creates a new Product struct and assigns the initial ownership to the msg.sender (Manufacturer).
+     * @param prodId Unique identifier for the product batch.
+     * @param ipfsHash hash of off-chain data pointing to the product's detailed information.
+     */
+    function createProduct(uint256 prodId, string memory ipfsHash) public roleCheker(Role.Producer){
+        // Intended Logic:
+        // 1. Verify the prodId does not already exist.
+        // 2. Create a new Product in the InProduction Status.
+        // 3. Emit the ProductRegistered event.
+    }
+
+
+    /**
+     * @notice Producer Make the produt ready for shipping to the warehouse.
+     * @param prodId Unique identifier for the product batch.
+     * @param ipfsHash hash of off-chain data pointing to the product's detailed information.
+     */
+    
+    function produce(uint256 prodId, string memory ipfsHash) public roleCheker(Role.Producer){
+        // Intended Logic:
+        // 1. Verify the prodId is in In_production Status.
+        // 2. Update the product status to ReadyToShip.
+        // 3. Emit the ProductReadyToShip event.
+    }
+    
+
+    /************************************************
+    * Distributor's Capabilities 
+    * (receiving from producer, quality check at warehouse, shipping, receiving the returned product from retailer)
+    *************************************************/
+
+
+    /************************************************
+    * Retailer's Capabilities 
+    * (receiving, quality check, returning to warehouse, putting on shelf, selling to consumer)
+    *************************************************/
+
+
+    /************************************************
+    * Consumer's Capabilities 
+    * (verifying product details, purchasing product)
+    *************************************************/
+
+
+}
